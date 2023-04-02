0711ebf55e5c762402a19ab90ec4fff1386ebe25
tatiana-clim <43815866+tcnaa-iscteiul@users.noreply.github.com>
2023-04-02 23:43:17 +0100

    Projeto criado

---
+++ ES-2023-2Sem-Terca-Feira-LEIPL-GrupoE/src/main/java/pt/iscte_iul/ista/ES_2023_2Sem_Terca_Feira_LEIPL_GrupoE/App.java
@@ -0,0 +1,13 @@
+package pt.iscte_iul.ista.ES_2023_2Sem_Terca_Feira_LEIPL_GrupoE;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
