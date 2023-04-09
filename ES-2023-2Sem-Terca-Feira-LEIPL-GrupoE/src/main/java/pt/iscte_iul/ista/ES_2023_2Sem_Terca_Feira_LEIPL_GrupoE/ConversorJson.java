e75b9896342d3c41ed5e0f8e15be81d6b88a947e
lfran@iscte-iul.pt <102904179+lfranyr@users.noreply.github.com>
2023-04-09 11:24:59 +0100

    Acrescentados comentários para geração de JavaDoc; Versão de jUnit actualizado par 5.9;Testes actualizados.

--- ES-2023-2Sem-Terca-Feira-LEIPL-GrupoE/src/main/java/pt/iscte_iul/ista/ES_2023_2Sem_Terca_Feira_LEIPL_GrupoE/ConversorJson.java
+++ ES-2023-2Sem-Terca-Feira-LEIPL-GrupoE/src/main/java/pt/iscte_iul/ista/ES_2023_2Sem_Terca_Feira_LEIPL_GrupoE/ConversorJson.java
@@ -1,3 +1,7 @@
+/**
+ * Classe ConversorJson responsável pela conversão de objetos Aula para formato JSON e vice-versa.
+ * Utiliza a biblioteca Gson do Google para realizar a conversão.
+ */
 package pt.iscte_iul.ista.ES_2023_2Sem_Terca_Feira_LEIPL_GrupoE;
 
 import com.google.gson.Gson;
@@ -12,16 +16,31 @@ import java.io.Writer;
 import java.lang.reflect.Type;
 import java.util.List;
 
+
 public class ConversorJson {
 
     private static final Gson gson = new GsonBuilder().setDateFormat("dd/MM/yyyy").setPrettyPrinting().create();
 
+    /**
+     * Método para gravar uma lista de objetos Aula em um arquivo JSON.
+     *
+     * @param aulas     Lista de objetos Aula a serem gravados no arquivo.
+     * @param filePath  Caminho do arquivo JSON onde os dados serão gravados.
+     * @throws IOException Se ocorrer um erro durante a gravação do arquivo.
+     */
     public static void gravarEmArquivoJSON(List<Aula> aulas, String filePath) throws IOException {
         try (Writer writer = new FileWriter(filePath)) {
             gson.toJson(aulas, writer);
         }
     }
 
+    /**
+     * Método para carregar uma lista de objetos Aula a partir de um arquivo JSON.
+     *
+     * @param filePath Caminho do arquivo JSON a ser lido.
+     * @return Uma lista de objetos Aula extraídos do arquivo JSON.
+     * @throws IOException Se ocorrer um erro durante a leitura do arquivo.
+     */
     public static List<Aula> carregarDeArquivoJSON(String filePath) throws IOException {
         try (Reader reader = new FileReader(filePath)) {
             Type listType = new TypeToken<List<Aula>>() {
