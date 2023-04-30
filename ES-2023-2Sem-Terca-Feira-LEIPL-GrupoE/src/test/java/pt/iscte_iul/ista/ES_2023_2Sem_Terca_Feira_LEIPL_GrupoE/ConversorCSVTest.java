491a963ed1b663941be81613b21aaf00d3f344b5
a23597-iscte <vitorhfteixeira@gmail.com>
2023-04-30 17:44:09 +0100

    Classe CriarNovoHorario e respetiva classe de teste implementadas

--- ES-2023-2Sem-Terca-Feira-LEIPL-GrupoE/src/test/java/pt/iscte_iul/ista/ES_2023_2Sem_Terca_Feira_LEIPL_GrupoE/ConversorCSVTest.java
+++ ES-2023-2Sem-Terca-Feira-LEIPL-GrupoE/src/test/java/pt/iscte_iul/ista/ES_2023_2Sem_Terca_Feira_LEIPL_GrupoE/ConversorCSVTest.java
@@ -1,9 +1,7 @@
 package pt.iscte_iul.ista.ES_2023_2Sem_Terca_Feira_LEIPL_GrupoE;
 
 import static org.junit.jupiter.api.Assertions.*;
-
 import org.junit.jupiter.api.Test;
-
 import com.opencsv.exceptions.CsvException;
 
 /**
@@ -12,8 +10,9 @@ import com.opencsv.exceptions.CsvException;
  *
  */
 
-class ConversorCSVTest {
-
+public class ConversorCSVTest {
+	
+	
 	/*@Test
 	void test() {
 		fail("Not yet implemented");
@@ -22,9 +21,9 @@ class ConversorCSVTest {
 	@Test
 	public void testCSV() throws CsvException {
 		
-		Horario h = ConversorCSV.lerCSVParaEstrutura("C:\\Users\\vitor\\Documents\\Licenciatura em Engenharia Informatica ISCTE\\2022-2023\\ES\\Projeto\\OLD\\horario-exemplo.csv");
-		ConversorCSV.escreveCSV(h, "C:\\Users\\vitor\\Documents\\Licenciatura em Engenharia Informatica ISCTE\\2022-2023\\ES\\Projeto\\OLD\\horario-teste.csv");
-		Horario h1 = ConversorCSV.lerCSVParaEstrutura("C:\\Users\\vitor\\Documents\\Licenciatura em Engenharia Informatica ISCTE\\2022-2023\\ES\\Projeto\\OLD\\horario-teste.csv");
+		Horario h = ConversorCSV.lerCSVParaEstrutura("horario_bemFormatado_Final.csv");
+		ConversorCSV.escreveCSV(h, "horario-teste.csv");
+		Horario h1 = ConversorCSV.lerCSVParaEstrutura("horario-teste.csv");
 		
 		assertEquals(h.getAulas().get(123).getCurso(),h1.getAulas().get(123).getCurso());
 	}
